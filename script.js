261b62fc492dfd989bbc404dca659eccea7a056c
Vital-uK <KolmykovVN@gmail.com>
2022-02-12 23:11:54 +0300

    optimized code

--- script.js
+++ script.js
@@ -2,52 +2,46 @@ const checkLength = function(string) {
     let result = string.length;
     if (string.includes('.')) result--;
     if (string.includes('-')) result--;
-    
+
+    return result;
+}
+
+const betterResult = function(result) {
+    result = +(result.toFixed(9));
+    if (checkLength(result.toString())>10) return result.toPrecision(10);
     return result;
 }
 
 const add = function(a,b) {
     let result = a + b;
-    result = +(result.toFixed(9));
-    if (checkLength(result.toString())>10) return result.toPrecision(10);
-    return result;    
+    return betterResult(result);    
 }
 
 const subtract = function(a,b) {
     let result = a-b;
-    result = +(result.toFixed(9));
-    if (checkLength(result.toString())>10) return result.toPrecision(10);
-    return result;         
+    return betterResult(result);         
 }
 
 const multiply = function(a,b) {
     let result = a*b;
-    result = +(result.toFixed(9));
-    if (checkLength(result.toString())>10) return result.toPrecision(10);
-    return result;          
+    return betterResult(result);           
 }
 
 const divide = function(a,b) {
     let result
     if (b != 0) {
         result = a/b;
-        result = +(result.toFixed(9));
-        if (checkLength(result.toString())>10) return result.toPrecision(10);
-        return result;          
+        return betterResult(result);           
     }
     return 'ERROR';        
 }
 
 const toPower = function(a,b) {
     let result = a**b;
-    result = +(result.toFixed(9));
-    if (checkLength(result.toString())>10) return result.toPrecision(10);
-    return result;        
+    return betterResult(result);         
 }
 
 const changeSign = function(a) {
     result = -a
-    result = +(result.toFixed(9));
-    if (checkLength(result.toString())>10) return result.toPrecision(10);
-    return result;         
+    return betterResult(result);          
 }
